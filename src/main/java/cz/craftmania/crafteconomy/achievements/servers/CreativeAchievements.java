46bdb6699143550c138451d5f4dc0037dc86aa70
MrWakeCZ <joseff.kral@gmail.com>
2019-09-06 14:28:55 +0200

    Oprava chybějících názvů achievementů
    * všechny byly [Unknown] jako default

--- src/main/java/cz/craftmania/crafteconomy/achievements/servers/CreativeAchievements.java
+++ src/main/java/cz/craftmania/crafteconomy/achievements/servers/CreativeAchievements.java
@@ -14,25 +14,25 @@ public class CreativeAchievements {
     }
 
     public void load() {
-        this.rewards.add(new AchievementReward("common_breaks_bedrock_1").setRarity(Rarity.COMMON));
-        this.rewards.add(new AchievementReward("rare_breaks_grass_500").setRarity(Rarity.RARE));
-        this.rewards.add(new AchievementReward("rare_connections_1").setRarity(Rarity.RARE).overrideExperienceValue(1000));
-        this.rewards.add(new AchievementReward("common_custom_playedtime_creative_1").setRarity(Rarity.COMMON));
-        this.rewards.add(new AchievementReward("rare_custom_playedtime_creative_12").setRarity(Rarity.RARE));
-        this.rewards.add(new AchievementReward("epic_custom_playedtime_creative_24").setRarity(Rarity.EPIC));
-        this.rewards.add(new AchievementReward("legendary_custom_playedtime_creative_168").setRarity(Rarity.LEGENDARY));
-        this.rewards.add(new AchievementReward("legendary_custom_playedtime_creative_720").setRarity(Rarity.LEGENDARY));
-        this.rewards.add(new AchievementReward("common_deaths_1").setRarity(Rarity.COMMON));
-        this.rewards.add(new AchievementReward("epic_deaths_100").setRarity(Rarity.EPIC));
-        this.rewards.add(new AchievementReward("epic_distancehorse_10000").setRarity(Rarity.EPIC));
-        this.rewards.add(new AchievementReward("epic_distancepig_1000").setRarity(Rarity.EPIC));
-        this.rewards.add(new AchievementReward("common_fireworks_50").setRarity(Rarity.COMMON));
-        this.rewards.add(new AchievementReward("common_musicdiscs_1").setRarity(Rarity.COMMON));
-        this.rewards.add(new AchievementReward("common_places_grass_500").setRarity(Rarity.COMMON));
-        this.rewards.add(new AchievementReward("common_places_stone_50").setRarity(Rarity.COMMON));
-        this.rewards.add(new AchievementReward("common_places_stone_500").setRarity(Rarity.COMMON));
-        this.rewards.add(new AchievementReward("rare_places_stone_5000").setRarity(Rarity.RARE));
-        this.rewards.add(new AchievementReward("rare_places_tnt_1").setRarity(Rarity.RARE));
+        this.rewards.add(new AchievementReward("common_breaks_bedrock_1").setRarity(Rarity.COMMON).setName("Bedrocker"));
+        this.rewards.add(new AchievementReward("rare_breaks_grass_500").setRarity(Rarity.RARE).setName("Survival"));
+        this.rewards.add(new AchievementReward("rare_connections_1").setRarity(Rarity.RARE).overrideExperienceValue(1000).setName("Vítej na Creativu!"));
+        this.rewards.add(new AchievementReward("common_custom_playedtime_creative_1").setRarity(Rarity.COMMON).setName("První hodina"));
+        this.rewards.add(new AchievementReward("rare_custom_playedtime_creative_12").setRarity(Rarity.RARE).setName("Půl dne online"));
+        this.rewards.add(new AchievementReward("epic_custom_playedtime_creative_24").setRarity(Rarity.EPIC).setName("Celý den online"));
+        this.rewards.add(new AchievementReward("legendary_custom_playedtime_creative_168").setRarity(Rarity.LEGENDARY).setName("Týden online"));
+        this.rewards.add(new AchievementReward("legendary_custom_playedtime_creative_720").setRarity(Rarity.LEGENDARY).setName("Měsíc online"));
+        this.rewards.add(new AchievementReward("common_deaths_1").setRarity(Rarity.COMMON).setName("Undead I"));
+        this.rewards.add(new AchievementReward("epic_deaths_100").setRarity(Rarity.EPIC).setName("Undead II"));
+        this.rewards.add(new AchievementReward("epic_distancehorse_10000").setRarity(Rarity.EPIC).setName("Horse Distance"));
+        this.rewards.add(new AchievementReward("epic_distancepig_1000").setRarity(Rarity.EPIC).setName("Pig Distance"));
+        this.rewards.add(new AchievementReward("common_fireworks_50").setRarity(Rarity.COMMON).setName("Pyrotechnic"));
+        this.rewards.add(new AchievementReward("common_musicdiscs_1").setRarity(Rarity.COMMON).setName("Jukeboxer"));
+        this.rewards.add(new AchievementReward("common_places_grass_500").setRarity(Rarity.COMMON).setName("Grass Master"));
+        this.rewards.add(new AchievementReward("common_places_stone_50").setRarity(Rarity.COMMON).setName("Place Stone"));
+        this.rewards.add(new AchievementReward("common_places_stone_500").setRarity(Rarity.COMMON).setName("Více Stonuuu"));
+        this.rewards.add(new AchievementReward("rare_places_stone_5000").setRarity(Rarity.RARE).setName("Stone Master"));
+        this.rewards.add(new AchievementReward("rare_places_tnt_1").setRarity(Rarity.RARE).setName("Silent Explosion"));
 
     }
 }
--- src/main/java/cz/craftmania/crafteconomy/achievements/servers/GlobalAchievements.java
+++ src/main/java/cz/craftmania/crafteconomy/achievements/servers/GlobalAchievements.java
@@ -14,16 +14,20 @@ public class GlobalAchievements {
     }
 
     public void load() {
-        this.rewards.add(new AchievementReward("common_custom_bugs_1").setRarity(Rarity.COMMON));
-        this.rewards.add(new AchievementReward("rare_custom_bugs_2").setRarity(Rarity.RARE));
-        this.rewards.add(new AchievementReward("epic_custom_bugs_5").setRarity(Rarity.EPIC));
-        this.rewards.add(new AchievementReward("legendary_custom_bugs_10").setRarity(Rarity.LEGENDARY));
-        this.rewards.add(new AchievementReward("mythic_custom_bugs_20").setRarity(Rarity.MYTHIC));
-        this.rewards.add(new AchievementReward("common_custom_votes_1").setRarity(Rarity.COMMON));
-        this.rewards.add(new AchievementReward("common_custom_votes_50").setRarity(Rarity.COMMON));
-        this.rewards.add(new AchievementReward("rare_custom_votes_250").setRarity(Rarity.RARE));
-        this.rewards.add(new AchievementReward("epic_custom_votes_1000").setRarity(Rarity.EPIC));
-        this.rewards.add(new AchievementReward("legendary_custom_votes_10000").setRarity(Rarity.LEGENDARY));
-        this.rewards.add(new AchievementReward("mythic_custom_votes_100000").setRarity(Rarity.MYTHIC));
+        // Bug achievementy
+        this.rewards.add(new AchievementReward("common_custom_bugs_1").setRarity(Rarity.COMMON).setName("Debugger I"));
+        this.rewards.add(new AchievementReward("rare_custom_bugs_2").setRarity(Rarity.RARE).setName("Debugger II"));
+        this.rewards.add(new AchievementReward("epic_custom_bugs_5").setRarity(Rarity.EPIC).setName("Debugger III"));
+        this.rewards.add(new AchievementReward("legendary_custom_bugs_10").setRarity(Rarity.LEGENDARY).setName("Debugger IV"));
+        this.rewards.add(new AchievementReward("mythic_custom_bugs_20").setRarity(Rarity.MYTHIC).setName("Master Debugger"));
+
+        // Vote achievementy
+        //TODO: Funkčnost
+        this.rewards.add(new AchievementReward("common_custom_votes_1").setRarity(Rarity.COMMON).setName("Fanoušek serveru"));
+        this.rewards.add(new AchievementReward("common_custom_votes_50").setRarity(Rarity.COMMON).setName("Velký fanda"));
+        this.rewards.add(new AchievementReward("rare_custom_votes_250").setRarity(Rarity.RARE).setName("Větší fanda"));
+        this.rewards.add(new AchievementReward("epic_custom_votes_1000").setRarity(Rarity.EPIC).setName("Epický fanoušek"));
+        this.rewards.add(new AchievementReward("legendary_custom_votes_10000").setRarity(Rarity.LEGENDARY).setName("Legendární fanoušek"));
+        this.rewards.add(new AchievementReward("mythic_custom_votes_100000").setRarity(Rarity.MYTHIC).setName("Pravá legenda!"));
     }
 }
