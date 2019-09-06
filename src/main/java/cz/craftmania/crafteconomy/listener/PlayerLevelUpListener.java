7f5a36e48c2c33269fc6b46f65a75afa184e1420
MrWakeCZ <joseff.kral@gmail.com>
2019-09-07 00:00:16 +0200

    Akce při levelupu podle levels

--- src/main/java/cz/craftmania/crafteconomy/listener/PlayerLevelUpListener.java
+++ src/main/java/cz/craftmania/crafteconomy/listener/PlayerLevelUpListener.java
@@ -1,9 +1,13 @@
 package cz.craftmania.crafteconomy.listener;
 
+import cz.craftmania.crafteconomy.Main;
 import cz.craftmania.crafteconomy.events.PlayerLevelUpEvent;
 import cz.craftmania.crafteconomy.managers.BasicManager;
 import cz.craftmania.crafteconomy.managers.ProprietaryManager;
 import cz.craftmania.crafteconomy.objects.CraftPlayer;
+import cz.craftmania.crafteconomy.objects.LevelReward;
+import cz.craftmania.crafteconomy.utils.Logger;
+import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -20,9 +24,11 @@ public class PlayerLevelUpListener implements Listener {
         BasicManager basicManager = new BasicManager();
         long currentLevel = cp.getLevelByType(basicManager.getLevelByServer());
 
+        Logger.info("Hrac: " + p.getName() + ", dostal level up na: " + currentLevel);
+
         ProprietaryManager.getServerLevelRewardsList().forEach(level -> {
             if (level.getLevel() == currentLevel) {
-                //TODO: ACTION
+                doAction(level, p);
             }
         });
 
@@ -34,5 +40,28 @@ public class PlayerLevelUpListener implements Listener {
         p.sendMessage("");
         p.sendMessage("§9\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac");
 
+    }
+
+    private void doAction(LevelReward level, Player player) {
+
+        if (level == null || player == null) {
+            return;
+        }
+
+        if (!level.getPermissions().isEmpty()) {
+            level.getPermissions().forEach(permission -> {
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getName() + " permission set " + permission + " " + Main.getServerType().name().toLowerCase());
+            });
+        }
+
+        // Notify
+        player.sendMessage("§b\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac");
+        player.sendMessage("");
+        player.sendMessage("§9§lOdmena za level: §f" + level.getLevel());
+        level.getRewardDescription().forEach(description -> {
+            player.sendMessage("§7" + description);
+        });
+        player.sendMessage("");
+        player.sendMessage("§b\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac\u25ac");
     }
 }
