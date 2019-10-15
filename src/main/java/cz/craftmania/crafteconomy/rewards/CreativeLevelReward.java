34ed9048c51482b428ed6ea498900d8b301b9c93
MrWakeCZ <joseff.kral@gmail.com>
2019-10-15 02:12:06 +0200

    Nové rewardy pro Creative

--- src/main/java/cz/craftmania/crafteconomy/rewards/CreativeLevelReward.java
+++ src/main/java/cz/craftmania/crafteconomy/rewards/CreativeLevelReward.java
@@ -25,11 +25,17 @@ public class CreativeLevelReward {
                 .setDescription("§7Přístup k používání Fish,", "§7Cod, Salmon a Pufferfish Eggs.")
                 .setRewardDescription("§7Nyní můžeš spawnovat rybičky pomocí spawn eggů!")
                 .setPermissions(
-                        "rc.bypass.disable.interacting.in-hand.COD_SPAWN_EGG",
-                        "rc.bypass.disable.interacting.in-hand.SALMON_SPAWN_EGG",
-                        "rc.bypass.disable.interacting.in-hand.PUFFERFISH_SPAWN_EGG",
-                        "rc.bypass.disable.interacting.in-hand.TROPICAL_FISH_SPAWN_EGG"
+                        "rc.bypass.confiscate.items.material.COD_SPAWN_EGG",
+                        "rc.bypass.confiscate.items.material.SALMON_SPAWN_EGG",
+                        "rc.bypass.confiscate.items.material.PUFFERFISH_SPAWN_EGG",
+                        "rc.bypass.confiscate.items.material.TROPICAL_FISH_SPAWN_EGG"
                 ));
+
+        // LEVEL 5
+        this.list.add(new LevelReward(5).setName("Přístup k Cow Eggs")
+                .setDescription("§7Přístup k používání Cow Eggs.")
+                .setRewardDescription("§7Nyní můžeš spawnovat kravičky pomocí spawn eggů!")
+                .setPermissions("rc.bypass.confiscate.items.material.COW_SPAWN_EGG"));
 
         // LEVEL 6
         this.list.add(new LevelReward(6).setName("Stažení pozemku")
@@ -41,13 +47,43 @@ public class CreativeLevelReward {
         this.list.add(new LevelReward(7).setName("Přístup k Pig Eggs")
                 .setDescription("§7Přístup k používání Pig Eggs.")
                 .setRewardDescription("§7Nyní můžeš spawnovat prasátka pomocí spawn eggů!")
-                .setPermissions("rc.bypass.disable.interacting.in-hand.PIG_SPAWN_EGG"));
+                .setPermissions("rc.bypass.confiscate.items.material.PIG_SPAWN_EGG"));
+
+        // LEVEL 8
+        this.list.add(new LevelReward(8).setName("Přístup k Llama Eggs")
+                .setDescription("§7Přístup k používání Llama Eggs.")
+                .setRewardDescription("§7Nyní můžeš spawnovat llamy pomocí spawn eggů!")
+                .setPermissions("rc.bypass.confiscate.items.material.TRADER_LLAMA_SPAWN_EGG", "rc.bypass.confiscate.items.material.LLAMA_SPAWN_EGG"));
 
         // LEVEL 9
         this.list.add(new LevelReward(9).setName("Přístip k Fox Eggs")
                 .setDescription("§7Přístup k použivání Fox Eggs.")
                 .setRewardDescription("§7Nyní si můžeš spawnout lišku", "§7pomocí Fox spawn Eggu.")
-                .setPermissions("rc.bypass.disable.interacting.in-hand.FOX_SPAWN_EGG"));
+                .setPermissions("rc.bypass.confiscate.items.material.FOX_SPAWN_EGG"));
+
+        // LEVEL 11
+        this.list.add(new LevelReward(11).setName("Přístip k Panda Eggs")
+                .setDescription("§7Přístup k použivání Panda Eggs.")
+                .setRewardDescription("§7Nyní si můžeš spawnout pandu", "§7pomocí Panda spawn Eggu.")
+                .setPermissions("rc.bypass.confiscate.items.material.PANDA_SPAWN_EGG"));
+
+        // LEVEL 12
+        this.list.add(new LevelReward(12).setName("Přístip k Polar Bear Eggs")
+                .setDescription("§7Přístup k použivání Polar Bear Eggs.")
+                .setRewardDescription("§7Nyní si můžeš spawnout medvídka", "§7pomocí Polar Bear spawn Eggu.")
+                .setPermissions("rc.bypass.confiscate.items.material.POLAR_BEAR_SPAWN_EGG"));
+
+        // LEVEL 13
+        this.list.add(new LevelReward(13).setName("Přístip k Wolf & Cat Eggs")
+                .setDescription("§7Přístup k použivání Wolf & Cat Eggs.")
+                .setRewardDescription("§7Nyní si můžeš spawnout psa a nebo kočku", "§7pomocí Wolf & Cat spawn Eggu.")
+                .setPermissions("rc.bypass.confiscate.items.material.WOLF_SPAWN_EGG", "rc.bypass.confiscate.items.material.CAT_SPAWN_EGG"));
+
+        // LEVEL 14
+        this.list.add(new LevelReward(14).setName("Přístip k Parrot Eggs")
+                .setDescription("§7Přístup k použivání Parrot Eggs.")
+                .setRewardDescription("§7Nyní si můžeš spawnout papouška", "§7pomocí Parrot spawn Eggu.")
+                .setPermissions("rc.bypass.confiscate.items.material.PARROT_SPAWN_EGG"));
 
         /*// LEVEL 10
         this.list.add(new LevelReward(10).setName("Disguise přeměny (Zvířata)")
