0c2b130cab2fe7499d4329b8fada5f4269dce83a
MrWakeCZ <joseff.kral@gmail.com>
2019-11-29 02:07:00 +0100

    Odstraněný fix pro Vanillu levels

--- src/main/java/cz/craftmania/crafteconomy/listener/PlayerJoinListener.java
+++ src/main/java/cz/craftmania/crafteconomy/listener/PlayerJoinListener.java
@@ -1,11 +1,8 @@
 package cz.craftmania.crafteconomy.listener;
 
 import cz.craftmania.crafteconomy.Main;
-import cz.craftmania.crafteconomy.api.AchievementPointsAPI;
 import cz.craftmania.crafteconomy.managers.BasicManager;
 import cz.craftmania.crafteconomy.objects.CraftPlayer;
-import cz.craftmania.crafteconomy.utils.ServerType;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -28,17 +25,6 @@ public class PlayerJoinListener implements Listener {
 
         // Zakladni nacteni dat do cache a vytvoření objektu
         CraftPlayer craftPlayer = BasicManager.loadPlayerData(player);
-
-        // Ignorování unknown serverů
-        if (Main.getServerType() == ServerType.UNKNOWN) {
-            return;
-        }
-
-        if (Main.getServerType() == ServerType.VANILLA) {
-            if (craftPlayer.getLevelByType(bm.getLevelByServer()) < 22 && player.hasPermission("lands.lands.3")) {
-                bm.removePlayerReward(player, 22);
-            }
-        }
     }
 
     @EventHandler(ignoreCancelled = true)
