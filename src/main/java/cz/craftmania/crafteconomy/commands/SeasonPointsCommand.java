f4fe0a8014f00ba8dbea66bd89e34568ff5b5112
MrWakeCZ <joseff.kral@gmail.com>
2022-05-10 22:57:27 +0200

    New: Podpora pro přidávání SeasonPointu z konzole

--- src/main/java/cz/craftmania/crafteconomy/commands/SeasonPointsCommand.java
+++ src/main/java/cz/craftmania/crafteconomy/commands/SeasonPointsCommand.java
@@ -5,6 +5,7 @@ import co.aikar.commands.CommandHelp;
 import co.aikar.commands.annotation.*;
 import cz.craftmania.crafteconomy.api.EconomyAPI;
 import cz.craftmania.crafteconomy.managers.BasicManager;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -26,5 +27,20 @@ public class SeasonPointsCommand extends BaseCommand {
     public void showSeasonPoints(CommandSender sender) {
         if (sender instanceof Player)
             sender.sendMessage("§e§l[*] §eAktuálně máš " + EconomyAPI.SEASON_POINTS.get((Player) sender) + " SeasonPoints.");
+    }
+
+    @Subcommand("add|give")
+    @CommandPermission("crafteconomy.admin")
+    @CommandCompletion("@players [pocet]")
+    @Syntax("[nick] [pocet]")
+    public void adminSeasonPointsTokens(CommandSender sender, String editedPlayer, long tokensToAdd) {
+        Player p = Bukkit.getPlayer(editedPlayer);
+        if (p != null) {
+            EconomyAPI.SEASON_POINTS.give(p, tokensToAdd);
+            sender.sendMessage("§e§l[*] §ePřidal jsi hráči §f" + editedPlayer + " §7- §b" + tokensToAdd + " SP.");
+        } else {
+            EconomyAPI.SEASON_POINTS.give(editedPlayer, tokensToAdd);
+            sender.sendMessage("§e§l[*] §ePřidal jsi hráči §f" + editedPlayer + " §7- §b" + tokensToAdd + " SP.");
+        }
     }
 }
