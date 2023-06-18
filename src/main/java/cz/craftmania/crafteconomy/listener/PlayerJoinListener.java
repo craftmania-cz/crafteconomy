8f0658afd334ab4dacd88ae1e7eb65e69db439ac
MrWakeCZ <joseff.kral@gmail.com>
2023-06-19 01:49:39 +0200

    Update: Oprava odměny za 7. lvl pro Jobs na Survival: Classic

--- src/main/java/cz/craftmania/crafteconomy/listener/PlayerJoinListener.java
+++ src/main/java/cz/craftmania/crafteconomy/listener/PlayerJoinListener.java
@@ -98,20 +98,6 @@ public class PlayerJoinListener implements Listener {
             }
         }
 
-        if (Main.getServerType() == ServerType.SKYBLOCK_117) {
-            if (bm.getCraftPlayer(player).getLevelByType(LevelType.SKYBLOCK_117_LEVEL) >= 25 && !player.hasPermission("cmi.command.flightcharge")) {
-                bm.givePlayerManualLevelReward(player, 25, true);
-            }
-
-            if (bm.getCraftPlayer(player).getLevelByType(LevelType.SKYBLOCK_117_LEVEL) >= 47 && !player.hasPermission("shopguiplus.item.spawners.12")) {
-                bm.givePlayerManualLevelReward(player, 47, true);
-            }
-
-            if (bm.getCraftPlayer(player).getLevelByType(LevelType.SKYBLOCK_117_LEVEL) >= 50 && !player.hasPermission("shopguiplus.item.spawners.13")) {
-                bm.givePlayerManualLevelReward(player, 50, true);
-            }
-        }
-
         if (Main.getServerType() == ServerType.CREATIVE) {
             if (bm.getCraftPlayer(player).getLevelByType(LevelType.CREATIVE_LEVEL) >= 15 && !player.hasPermission("plots.set.biome")) {
                 bm.givePlayerManualLevelReward(player, 15, true);
@@ -128,6 +114,20 @@ public class PlayerJoinListener implements Listener {
                         "SURVIVAL",
                         "Oprava: Odměna za 21. survival level",
                         "§7Nyní můžeš použít příkaz §e/fly §7k neomezenému lítání."
+                );
+            }
+        }
+
+        if (Main.getServerType() == ServerType.SURVIVAL_117) {
+            if (bm.getCraftPlayer(player).getLevelByType(LevelType.SURVIVAL_117_LEVEL) >= 7 && !player.hasPermission("craftmanager.jobs.allow-hunter")) {
+                bm.givePlayerManualLevelReward(player, 7, false);
+                NotificationsAPI.Companion.createNotificationByUUID(
+                        player.getUniqueId(),
+                        NotificationType.SERVER,
+                        NotificationPriority.NORMAL,
+                        "SURVIVAL",
+                        "Oprava: Odměna za 7. survival level",
+                        "§7Nyní můžeš pracovat jako §eHunter §7a zabíjet moby."
                 );
             }
         }
