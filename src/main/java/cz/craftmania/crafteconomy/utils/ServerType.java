6308c283d004f6d6c4c6d33af3179917c45db012
MrWakeCZ <joseff.kral@gmail.com>
2024-01-07 23:48:09 +0100

    Update: Všechny staré servery jsou deprecated

--- src/main/java/cz/craftmania/crafteconomy/utils/ServerType.java
+++ src/main/java/cz/craftmania/crafteconomy/utils/ServerType.java
@@ -3,17 +3,25 @@ package cz.craftmania.crafteconomy.utils;
 public enum ServerType {
 
     SURVIVAL_117("survival"),
+    @Deprecated
     SURVIVAL_118("survival_118"),
+    @Deprecated
     SKYBLOCK_117("skyblock"),
     SKYBLOCK_118("skyblock_118"),
     CREATIVE("creative"),
+    @Deprecated
     PRISON("prison"),
+    @Deprecated
     VANILLA("vanilla"),
+    @Deprecated
     SKYCLOUD("skycloud"),
     LOBBY("lobby"),
     EVENT_SERVER("event_server"),
+    @Deprecated
     SKYGRID("skygrid"),
+    @Deprecated
     HARDCORE_VANILLA("hvanilla"),
+    @Deprecated
     ANARCHY("anarchy"),
 
     /**
